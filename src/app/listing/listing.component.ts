4b289368303cc18e2527690e85032c690a48f4b5
Andrew William Thomas <andrew.thomas640@gmail.com>
2024-03-13 15:59:44 +0000

    Changes

--- src/app/listing/listing.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'app-listing',
-  templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.scss'],
-})
-export class ListingComponent {}
